bcf2b8365ef736cbb2e3a9729cb0a2526e9a0bde
Guest User <Guest@Epicodus-9C.local>
2018-04-16 16:35:42 -0700

    initial

---
+++ src/bizlogic.js
@@ -0,0 +1,7 @@
+// export function Entry(heading, paragraph) {
+//   this.heading = heading;
+//   this.paragraph = paragraph;
+// }
+//
+// export let heading = document.getElementById('title').value;
+// export let paragraph = document.getElementById('paragraph').value;
---
+++ src/uilogic.js
@@ -0,0 +1,16 @@
+// import { Entry } from './bizlogic.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+// var heading = require('./bizlogic.js');
+// var paragraph = require('./bizlogic.js');
+
+$(document).ready(function() {
+  // $('form').submit(function(event) {
+  //   event.preventDefault();
+  //   var newEntry = new Entry(heading, paragraph);
+  //   $('#title-output').text(newEntry.heading);
+  //   $('#paragraph-output').text(newEntry.paragraph);
+  // });
+});
